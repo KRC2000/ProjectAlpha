eee7ce08765e190d2dbfca03809df4d3b2e129ca
Roman Kovalov <POMKOV2000@gmail.com>
2021-04-01 14:27:03 +0300

    virtual void update() added

--- ProjectAlpha/GUI_Element.h
+++ ProjectAlpha/GUI_Element.h
@@ -1,6 +1,7 @@
 #pragma once
 //#include "PrecompiledHeaders.h"
 #include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Window/Event.hpp>
 #include <vector>
 
 class GUI_Element: public sf::Drawable
@@ -10,6 +11,7 @@ public:
 	virtual ~GUI_Element() {};
 
 	virtual void assignTextureRes(std::vector<sf::Texture>& textureResourcesVec) = 0;
+	virtual void update(sf::Event& event, sf::RenderWindow& window, sf::View& view) = 0;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override = 0;
 };
 
