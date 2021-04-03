b871bfd62cd2ef62dcafafcfe03ec0860e280e4b
Roman Kovalov <POMKOV2000@gmail.com>
2021-04-03 07:40:40 +0300

    Input registration methods updated due to rework of IEC

--- ProjectAlpha/Game.cpp
+++ ProjectAlpha/Game.cpp
@@ -34,76 +34,36 @@ void Game::processing()
 
 void Game::update()
 {
-	
-	Event event;
-	while (window.pollEvent(event))
+	while (window.pollEvent(iec.event))
 	{
-		if (event.type == Event::Closed)
+		if (iec.event.type == Event::Closed)
 			window.close();
-		if (event.type == Event::MouseWheelMoved)
+		if (iec.event.type == Event::MouseWheelMoved)
 		{
-			iec.mouseWheelScrolling(event.mouseWheel.delta);
-			//cout << iec.mouseWheelDelta << endl;
+			iec.receiveMouseWheelInput(iec.event.mouseWheel.delta);
 		}
-		if (event.type == Event::MouseButtonPressed)
+		if (iec.event.type == Event::MouseButtonPressed)
 		{
-			if (event.key.code == Mouse::Left) iec.LMBclick();
-			if (event.key.code == Mouse::Right) iec.RMBclick();
+			iec.receiveMouseInput(iec.event.mouseButton.button, IEC::KeyState::JUSTPRESSED);
 		}
-		if (event.type == Event::MouseButtonReleased)
+		if (iec.event.type == Event::MouseButtonReleased)
 		{
-			if (event.key.code == Mouse::Left) iec.LMBrelease();
-			if (event.key.code == Mouse::Right) iec.RMBrelease();
+			iec.receiveMouseInput(iec.event.mouseButton.button, IEC::KeyState::JUSTRELEASED);
 		}
-		if (event.type == Event::KeyPressed)
+		if (iec.event.type == Event::KeyPressed)
 		{
-			if (event.key.code == Keyboard::Up) iec.pressUp();
-			if (event.key.code == Keyboard::Down) iec.pressDown();
-			if (event.key.code == Keyboard::Left) iec.pressLeft();
-			if (event.key.code == Keyboard::Right) iec.pressRight();
-			if (event.key.code == Keyboard::Escape) iec.pressEscape();
-			if (event.key.code == Keyboard::W) iec.pressW();
-			if (event.key.code == Keyboard::A) iec.pressA();
-			if (event.key.code == Keyboard::S) iec.pressS();
-			if (event.key.code == Keyboard::D) iec.pressD();
-			if (event.key.code == Keyboard::R) iec.pressR();
-			if (event.key.code == Keyboard::K) iec.pressK();
-			if (event.key.code == Keyboard::X) iec.pressX();
-			if (event.key.code == Keyboard::Space) iec.pressSpace();
-			if (event.key.code == Keyboard::LShift) iec.pressLShift();
-			if (event.key.code == Keyboard::F5) iec.pressF5();
-			if (event.key.code == Keyboard::F6) iec.pressF6();
-			if (event.key.code == Keyboard::F11) iec.pressF11();
-			if (event.key.code == Keyboard::F12) iec.pressF12();
-			if (event.key.code == Keyboard::Equal) iec.pressEqual();
-			if (event.key.code == Keyboard::Dash) iec.pressDash();
+			iec.receiveKeyboardInput(iec.event.key.code, IEC::KeyState::JUSTPRESSED);
 		}
-		if (event.type == Event::KeyReleased)
+		if (iec.event.type == Event::KeyReleased)
 		{
-			if (event.key.code == Keyboard::Up) iec.releaseUp();
-			if (event.key.code == Keyboard::Down) iec.releaseDown();
-			if (event.key.code == Keyboard::Left) iec.releaseLeft();
-			if (event.key.code == Keyboard::Right) iec.releaseRight();
-			if (event.key.code == Keyboard::Escape) iec.releaseEscape();
-			if (event.key.code == Keyboard::W) iec.releaseW();
-			if (event.key.code == Keyboard::A) iec.releaseA();
-			if (event.key.code == Keyboard::S) iec.releaseS();
-			if (event.key.code == Keyboard::D) iec.releaseD();
-			if (event.key.code == Keyboard::R) iec.releaseR();
-			if (event.key.code == Keyboard::K) iec.releaseK();
-			if (event.key.code == Keyboard::X) iec.releaseX();
-			if (event.key.code == Keyboard::Space) iec.releaseSpace();
-			if (event.key.code == Keyboard::LShift) iec.releaseLShift();
-			if (event.key.code == Keyboard::F5) iec.releaseF5();
-			if (event.key.code == Keyboard::F6) iec.releaseF6();
-			if (event.key.code == Keyboard::F11) iec.releaseF11();
-			if (event.key.code == Keyboard::F12) iec.releaseF12();
-			if (event.key.code == Keyboard::Equal) iec.releaseEqual();
-			if (event.key.code == Keyboard::Dash) iec.releaseDash();
+			iec.receiveKeyboardInput(iec.event.key.code, IEC::KeyState::JUSTRELEASED);
 		}
 
 	}
-	if (iec._Esc && currentScenePtr != &mainMenuScene) goToScene(MAINMENU_SCENE);
+
+	//if (iec._Esc && currentScenePtr != &mainMenuScene) goToScene(MAINMENU_SCENE);
+	if (iec.getKeyboardKeyState(Keyboard::Escape) && 
+		currentScenePtr != &mainMenuScene) goToScene(MAINMENU_SCENE);
 
 	goToScene(gameScene.update(iec, window, videoMode));
 	goToScene(mainMenuScene.update(iec, window, videoMode));
