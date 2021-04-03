7627feb90b098b9c2e621b24dbaaafe36bcaea28
Roman Kovalov <POMKOV2000@gmail.com>
2021-04-03 07:38:49 +0300

    Total rework, to access cycle time and mouseWheelDelta now used getters added KeyState enumerator, receiveKeyboardInput(), receiveMouseInput(), receiveMouseWheelInput(), eventExpire(), expireMouseWheelDelta()

--- ProjectAlpha/IEC.cpp
+++ ProjectAlpha/IEC.cpp
@@ -2,272 +2,46 @@
 
 
 
+
 IEC::IEC()
 {
+	
 }
 
-void IEC::LMBclick()
+void IEC::receiveKeyboardInput(Keyboard::Key keyCode, IEC::KeyState state)
 {
-	_LMB = true;
-	LMB = true;
+	keyboardKeysArr[keyCode] = state;
 }
 
-void IEC::LMBrelease()
+void IEC::receiveMouseInput(Mouse::Button buttonCode, IEC::KeyState state)
 {
-	LMB_ = true;
-	LMB = false;
+	mouseButtonsArr[buttonCode] = state;
 }
 
-void IEC::RMBclick()
+IEC::KeyState IEC::getKeyboardKeyState(Keyboard::Key keyCode)
 {
-	_RMB = true;
-	RMB = true;
+	return keyboardKeysArr[keyCode];
 }
 
-void IEC::RMBrelease()
+IEC::KeyState IEC::getMouseButtonState(Mouse::Button buttonCode)
 {
-	RMB_ = true;
-	RMB = false;
+	return mouseButtonsArr[buttonCode];
 }
 
-void IEC::pressUp()
+void IEC::eventExpire(Keyboard::Key keyCode)
 {
-	if (!Up) _Up = true;
-	Up = true;
+	if (keyboardKeysArr[keyCode] == KeyState::JUSTPRESSED)
+		keyboardKeysArr[keyCode] = KeyState::PRESSED;
+	if (keyboardKeysArr[keyCode] == KeyState::JUSTRELEASED)
+		keyboardKeysArr[keyCode] = KeyState::RELEASED;
 }
 
-void IEC::releaseUp()
+void IEC::eventExpire(Mouse::Button buttonCode)
 {
-	Up_ = true;
-	Up = false;
-}
-
-void IEC::pressDown()
-{
-	if (!Down) _Down = true;
-	Down = true;
-}
-
-void IEC::releaseDown()
-{
-	Down_ = true;
-	Down = false;
-}
-
-void IEC::pressLeft()
-{
-	if (!Left) _Left = true;
-	Left = true;
-}
-
-void IEC::releaseLeft()
-{
-	Left_ = true;
-	Left = false;
-}
-
-void IEC::pressRight()
-{
-	if (!Right) _Right = true;
-	Right = true;
-}
-
-void IEC::releaseRight()
-{
-	Right_ = true;
-	Right = false;
-}
-
-void IEC::pressEscape()
-{
-	if (!Esc) _Esc= true;
-	Esc = true;
-}
-
-void IEC::releaseEscape()
-{
-	Esc_ = true;
-	Esc = false;
-}
-
-void IEC::pressW()
-{
-	if (!W) _W = true;
-	W = true;
-}
-
-void IEC::releaseW()
-{
-	W_ = true;
-	W = false;
-}
-
-void IEC::pressA()
-{
-	if (!A) _A = true;
-	A = true;
-}
-
-void IEC::releaseA()
-{
-	A_ = true;
-	A = false;
-}
-
-void IEC::pressS()
-{
-	if (!S) _S = true;
-	S = true;
-}
-
-void IEC::releaseS()
-{
-	S_ = true;
-	S = false;
-}
-
-void IEC::pressD()
-{
-	if (!D) _D = true;
-	D = true;
-}
-
-void IEC::releaseD()
-{
-	D_ = true;
-	D = false;
-}
-
-void IEC::pressR()
-{
-	if (!R) _R = true;
-	R = true;
-}
-
-void IEC::releaseR()
-{
-	R_ = true;
-	R = false;
-}
-
-void IEC::pressK()
-{
-	if (!K) _K = true;
-	K = true;
-}
-
-void IEC::releaseK()
-{
-	K_ = true;
-	K = false;
-}
-
-void IEC::pressX()
-{
-	if (!X) _X = true;
-	X = true;
-}
-
-void IEC::releaseX()
-{
-	X_ = true;
-	X = false;
-}
-
-void IEC::pressSpace()
-{
-	if (!Space) _Space = true;
-	Space = true;
-}
-
-void IEC::releaseSpace()
-{
-	Space_ = true;
-	Space = false;
-}
-
-void IEC::pressF5()
-{
-	if (!F5) _F5 = true;
-	F5 = true;
-}
-
-void IEC::releaseF5()
-{
-	F5_ = true;
-	F5 = false;
-}
-
-void IEC::pressF6()
-{
-	if (!F6) _F6 = true;
-	F6 = true;
-}
-
-void IEC::releaseF6()
-{
-	F6_ = true;
-	F6 = false;
-}
-
-void IEC::pressF12()
-{
-	if (!F12) _F12 = true;
-	F12 = true;
-}
-
-void IEC::releaseF12()
-{
-	F12_ = true;
-	F12 = false;
-}
-
-void IEC::pressF11()
-{
-	if (!F11) _F11 = true;
-	F11 = true;
-}
-
-void IEC::releaseF11()
-{
-	F11_ = true;
-	F11 = false;
-}
-
-void IEC::pressLShift()
-{
-	if (!LShift) _LShift = true;
-	LShift = true;
-}
-
-void IEC::releaseLShift()
-{
-	LShift_ = true;
-	LShift = false;
-}
-
-void IEC::pressEqual()
-{
-	if (!equal) _equal = true;
-	equal = true;
-}
-
-void IEC::releaseEqual()
-{
-	equal_ = true;
-	equal = false;
-}
-
-void IEC::pressDash()
-{
-	if (!dash) _dash = true;
-	dash = true;
-}
-
-void IEC::releaseDash()
-{
-	dash_ = true;
-	dash = false;
+	if (mouseButtonsArr[buttonCode] == KeyState::JUSTPRESSED)
+		mouseButtonsArr[buttonCode] = KeyState::PRESSED;
+	if (mouseButtonsArr[buttonCode] == KeyState::JUSTRELEASED)
+		mouseButtonsArr[buttonCode] = KeyState::RELEASED;
 }
 
 // Возвращает глобальные координаты курсора в мире с данным видом(камерой)
@@ -291,7 +65,7 @@ Vector2f IEC::getMousePos(RenderWindow& window)
 	return pos;
 }
 
-void IEC::mouseWheelScrolling(int delta)
+void IEC::receiveMouseWheelInput(int delta)
 {
 	mouseWheelDelta = delta;
 }
@@ -304,53 +78,22 @@ void IEC::moveMouse(int x, int y)
 
 void IEC::eventUpdate()
 {
+	for (unsigned int i = 0; i < Keyboard::KeyCount; i++)
+	{
+		if (keyboardKeysArr[i] == KeyState::JUSTPRESSED)
+			keyboardKeysArr[i] = KeyState::PRESSED;
+		if (keyboardKeysArr[i] == KeyState::JUSTRELEASED)
+			keyboardKeysArr[i] = KeyState::RELEASED;
+	}
+	for (unsigned int i = 0; i < Mouse::ButtonCount; i++)
+	{
+		if (mouseButtonsArr[i] == KeyState::JUSTPRESSED)
+			mouseButtonsArr[i] = KeyState::PRESSED;
+		if (mouseButtonsArr[i] == KeyState::JUSTRELEASED)
+			mouseButtonsArr[i] = KeyState::RELEASED;
+	}
+
 	mouseWheelDelta = 0;
-	_LMB = false;
-	LMB_ = false;
-	_RMB = false;
-	RMB_ = false;
-	_Up = false;
-	Up_ = false;
-	_Down = false;
-	Down_ = false;
-	_Left = false;
-	Left_ = false;
-	_Right = false;
-	Right_ = false;
-
-	_Esc = false;
-	Esc_ = false;
-
-	_W = false;
-	W_ = false;
-	_A = false;
-	A_ = false;
-	_S = false;
-	S_ = false;
-	_D = false;
-	D_ = false;
-	_R = false;
-	R_ = false;
-	_K = false;
-	K_ = false;
-	_X = false;
-	X_ = false;
-	_F5 = false;
-	F5_ = false;
-	_F6 = false;
-	F6_ = false;
-	_F12 = false;
-	F12_ = false;
-	_F11 = false;
-	F11_ = false;
-	_Space = false;
-	Space_ = false;
-	_LShift = false;
-	LShift_ = false;
-	_equal = false;
-	equal_ = false;
-	_dash = false;
-	dash_ = false;
 	cycleTime = timer.restart();
 }
 
--- ProjectAlpha/IEC.h
+++ ProjectAlpha/IEC.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <array>
+#include <iostream>
 using namespace sf;
 using namespace std;
 
@@ -12,136 +14,56 @@ Call eventUpdate() at the end of every cycle
 */
 class IEC
 {
+public:
+	enum KeyState
+	{
+		RELEASED,
+		JUSTPRESSED,
+		JUSTRELEASED,
+		PRESSED,
+		AMOUNT
+	};
+private:
 	Mouse mouse;
 	Clock timer;
-	
-public:
+	KeyState keyboardKeysArr[Keyboard::KeyCount]{KeyState::RELEASED};
+	KeyState mouseButtonsArr[Mouse::ButtonCount]{KeyState::RELEASED};
+
 	Time cycleTime;
-	Time timePerFrame = seconds(1.f / 60.f);
 	int mouseWheelDelta;
+	//Time timePerFrame = seconds(1.f / 60.f);
+public:
 
-	bool _LMB = false;
-	bool LMB = false;
-	bool LMB_ = false;
-	bool _RMB = false;
-	bool RMB = false;
-	bool RMB_ = false;
-
-	bool Up = false;
-	bool _Up = false;
-	bool Up_ = false;
-	bool Down = false;
-	bool _Down = false;
-	bool Down_ = false;
-	bool Left = false;
-	bool _Left = false;
-	bool Left_ = false;
-	bool Right = false;
-	bool _Right = false;
-	bool Right_ = false;
-
-	bool Esc = false;
-	bool _Esc = false;
-	bool Esc_ = false;
-
-	bool W = false;
-	bool _W = false;
-	bool W_ = false;
-	bool A = false;
-	bool _A = false;
-	bool A_ = false;
-	bool S = false;
-	bool _S = false;
-	bool S_ = false;
-	bool D = false;
-	bool _D = false;
-	bool D_ = false;
-	bool R = false;
-	bool _R = false;
-	bool R_ = false;
-	bool K = false;
-	bool _K = false;
-	bool K_ = false;
-	bool X = false;
-	bool _X = false;
-	bool X_ = false;
-	bool Space = false;
-	bool _Space = false;
-	bool Space_ = false;
-	bool LShift = false;
-	bool _LShift = false;
-	bool LShift_ = false;
-
-	bool _F5 = false;
-	bool F5 = false;
-	bool F5_ = false;
-	bool _F6 = false;
-	bool F6 = false;
-	bool F6_ = false;
-	bool _F11 = false;
-	bool F11 = false;
-	bool F11_ = false;
-	bool _F12 = false;
-	bool F12 = false;
-	bool F12_ = false;
-
-	bool equal_ = false;
-	bool equal = false;
-	bool _equal = false;
-
-	bool dash_ = false;
-	bool dash = false;
-	bool _dash = false;
-
+	Event event;
 
 	IEC();
-	void LMBclick();
-	void LMBrelease();
-	void RMBclick();
-	void RMBrelease();
 
-	void pressUp();
-	void releaseUp();
-	void pressDown();
-	void releaseDown();
-	void pressLeft();
-	void releaseLeft(); 
-	void pressRight();
-	void releaseRight();
+	/////////////////////
+	///These methods must be called in while(window.pollEvent()) loop
+	//
+	void receiveKeyboardInput(Keyboard::Key keyCode, IEC::KeyState state);
+	void receiveMouseInput(Mouse::Button buttonCode, IEC::KeyState state);
+	void receiveMouseWheelInput(int delta);
+	//
+	/////////////////////
 
-	void pressEscape();
-	void releaseEscape();
 
-	void pressW();
-	void releaseW();
-	void pressA();
-	void releaseA();
-	void pressS();
-	void releaseS();
-	void pressD();
-	void releaseD();
-	void pressR();
-	void releaseR();
-	void pressK();
-	void releaseK();
-	void pressX();
-	void releaseX();
-	void pressSpace();
-	void releaseSpace();
-	void pressF5();
-	void releaseF5();
-	void pressF6();
-	void releaseF6();
-	void pressF12();
-	void releaseF12();
-	void pressF11();
-	void releaseF11();
-	void pressLShift();
-	void releaseLShift();
-	void pressEqual();
-	void releaseEqual();
-	void pressDash();
-	void releaseDash();
+	/////////////////////
+	///These methods are used to get input status of a key or button
+	//
+	IEC::KeyState getKeyboardKeyState(Keyboard::Key keyCode);
+	IEC::KeyState getMouseButtonState(Mouse::Button buttonCode);
+	int getMouseWheelDelta() { return mouseWheelDelta; };
+	//
+	/////////////////////
+
+
+	void eventExpire(Keyboard::Key keyCode);
+	void eventExpire(Mouse::Button buttonCode);
+	void expireMouseWheelDelta() { mouseWheelDelta = 0; };
+
+
+	Time getCycleTime() { return cycleTime; };
 
 	//Gets global position of mouse basics on view in the world
 	Vector2f getMousePos(RenderWindow& window, View view);
@@ -149,10 +71,17 @@ public:
 	//Gets local screen position
 	Vector2f getMousePos(RenderWindow& window);
 
-	void mouseWheelScrolling(int delta);
 
+	/// <summary>
+	/// Moves computer cursor 
+	/// </summary>
+	/// <param name="x">offset x</param>
+	/// <param name="y">offset y</param>
 	void moveMouse(int x, int y);
-	//каждый цикл сбрасывает флаги нажатия и отпускания мыши
+
+	/// <summary>
+	/// Must be called last in every loop
+	/// </summary>
 	void eventUpdate();
 };
 
